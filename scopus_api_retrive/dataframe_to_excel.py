3d7ae5b8a412f1a42963b06492ae929a462a4ce9
Sudipta Paul <sudipta116@gmail.com>
2018-08-10 12:07:01 +1000

    Pandas dataframe to excel file transformation script upload

---
+++ scopus_api_retrive/dataframe_to_excel.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug  9 16:38:31 2018
+
+@author: z3525552
+"""
+
+def save_excel(df, filename, sheetname):
+    
+    '''This helper function takes input dataframe, name of the excel file,
+    sheetname and save the file as specified in the directory 
+    print out the status''' 
+    import pandas as pd
+    
+    writer = pd.ExcelWriter(filename)
+    df.to_excel(writer,sheetname, index=False, encoding='utf-8')
+    writer.save()
+    
+    return print("Your Excel file is ready!")
