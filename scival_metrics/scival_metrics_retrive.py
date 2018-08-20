2f0a493f0ea317dc48ab8835f7881bc96fe1759d
Sudipta Paul <sudipta116@gmail.com>
2018-08-20 10:13:02 +1000

    general purpose API script

---
+++ scival_metrics/scival_metrics_retrive.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 30 14:26:00 2018
+
+@author: z3525552
+"""
+
+def scival_metrics(scopus_id):
+    
+    """Retriving SciVal default metrics using Scival API 
+    it takes API file in order to get api key and scopus_id for which 
+    return all default metrics displayed in Scival """
+    
+    import requests
+    import pandas as pd
+    pd.set_option('display.float_format', lambda x: '%.3f' % x)
+
+    assert isinstance(scopus_id, (str, int))
+    
+    base_url = "http://api.elsevier.com/analytics/scival/author/metrics?"
+    
+    query = {'metricTypes': """ScholarlyOutput,CitationCount,hIndices,
+             FieldWeightedCitationImpact,CitationsPerPublication,
+		         OutputsInTopCitationPercentiles,PublicationsInTopJournalPercentiles""",
+		        'byYear': 'false',
+		        'yearRange': '5yrsAndCurrent',
+            'authors': '%s' %(scopus_id) }  
+    
+    token = 'cf19ff27ef3c0d95f93c26947eb6533f'
+    header = {'Accept':'application/json', 'X-ELS-APIKey': token}
+
+    try:
+        response = requests.get(url=base_url, params=query,headers= header)
+        
+        if not response.status_code == 200:
+        
+            return response.status_code
+        
+        else:
+            
+            response_data = response.json()    
+            data = response_data['results'][0]['metrics']
+            df = pd.DataFrame.from_records(data)
+    
+            result = df[['metricType', 'value']]
+            metrics = result.fillna(0.0)
+    
+            return metrics
+    except:
+         return "HTTP requestes fail"
