212414a1455b9dff5e9328c7e86389881cd6969d
Sudipta Paul <sudipta116@gmail.com>
2018-08-22 13:32:14 +1000

    refactor code and updated comments

--- scival_metrics/scival_FWCI_self_cite.py
+++ scival_metrics/scival_FWCI_self_cite.py
@@ -5,44 +5,30 @@ Created on Tue Aug  7 15:08:27 2018
 @author: z3525552
 """
 
-def get_scival_fwci_selfcite(file, author_id):
+def get_scival_fwci_selfcite(author_id):
     
     
     ''' This function is used for retriving SciVal matric Field weighted 
         citiations impact. It takes elseiver API file and author id(SCOPUS_ID) 
         as inputs. Return FWCI of the author including selfcitations and 
-        journal impact type CiteScore. 
+        journal impact type CiteScore. '''
         
-        Valid HTTP requestes code other than 200 return the 
-        status codes.If http requests fail return 0''' 
     
-    import requests
-    from elsiver_api_retrive import get_elseiver_API
-    
+    from scival_author_metrics import api_query
+        
     assert type(author_id) is str
     
-    API = get_elseiver_API(file)
+    query = '''metricTypes=FieldWeightedCitationImpact&yearRange=5yrsAndCurrent
+    &includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&
+    journalImpactType=CiteScore&showAsFieldWeighted=true&indexType=hIndex&
+    authors=%s' %(author_id)'''
     
-    base_url ='http://api.elsevier.com/analytics/scival/author/metrics?'
-    query = 'metricTypes=FieldWeightedCitationImpact&yearRange=5yrsAndCurrent&includeSelfCitations=true&byYear=false&includedDocs=AllPublicationTypes&journalImpactType=CiteScore&showAsFieldWeighted=true&indexType=hIndex&authors=%s' %(author_id)
+    #HTTP request response object        
+    response = api_query(query)      
     
-    url = base_url + query
-    
-    header = {'Accept':'application/json', 'X-ELS-APIKey': API}
-    
-    try:
-        response = requests.get(url, headers=header)
-        if not response.status_code == 200:
-            
-            return response.status_code
-        else:
+    #FWCI data frame from data dictionary         
+    fwci = response['results'][0]['metrics'][0]['value']             
        
-            response_data = response.json()       
-             
-            fwci = response_data['results'][0]['metrics'][0]['value']             
-       
-                      
-            return fwci
-        
-    except:
-          return 0
+                     
+    return fwci
+  
