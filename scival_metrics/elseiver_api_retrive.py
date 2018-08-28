b4a311a73758d41ac135b9d1b7be4c8af3f26756
Sudipta Paul <sudipta116@gmail.com>
2018-08-28 12:34:43 +1000

    updated docstring

--- scival_metrics/elseiver_api_retrive.py
+++ scival_metrics/elseiver_api_retrive.py
@@ -5,11 +5,21 @@ Created on Fri Aug  3 15:04:49 2018
 @author: z3525552
 """
 
-def get_elseiver_API(filepath ='../elsevier_developer.txt'):
+def get_elseiver_API(filepath ='../elsevier_developer'):
     
     '''Read the Elsevier developer API key from source file
-       return API Key'''
-     
+       return API Key
+    
+    Parameters
+    ----------
+    filepath : str
+               API file location
+    
+    Returns
+    -------
+    token : str
+            API key string
+    ''' 
     
     with open(filepath) as f:
         token = f.read().strip()
