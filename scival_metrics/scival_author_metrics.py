dbd7186807c1e79a27ec08772dbb6ad1a40b475a
Sudipta Paul <sudipta116@gmail.com>
2018-08-28 12:27:42 +1000

    Docstring included in query script

--- scival_metrics/scival_author_metrics.py
+++ scival_metrics/scival_author_metrics.py
@@ -7,15 +7,41 @@ Created on Wed Aug 22 09:55:38 2018
 
 def api_query(query):
     
+    """
+    It takes http query as input 
+    detail specifications 
+    https://dev.elsevier.com/scival.html
+
+    return response_data 
+
+    Parameters
+    ----------
+    query : str
+            scival author metrics query
     
+    Returns
+    -------
+    status_code : int
+                  400 : Invalid Request 
+                  401 : Authentication Error
+                  403 : Authorization/Entitlements Error
+                  405 : Invalid HTTP Method
+                  406 : Invalid Mime Type
+                  429 : Quota Exceeded
+                  500 : Generic Error
+    
+    response_data : dict
+                  response data dictionary 
+    
+    Raises: int    
+            0 : http request fail
+    """
     import requests
     from elseiver_api_retrive import get_elseiver_API
-    
     
     key = get_elseiver_API('../elsevier_developer')
     
     base_url ='http://api.elsevier.com/analytics/scival/author/metrics?'
-    
     
     header = {'Accept':'application/json', 'X-ELS-APIKey': key}
     
@@ -28,8 +54,7 @@ def api_query(query):
         else:
        
             response_data = response.json()       
-            
-                      
+                                  
             return response_data
         
     except:
