e28e983d01fe245998f68388ffbbc28fa8389cc5
Sudipta Paul <sudipta116@gmail.com>
2018-10-08 15:22:07 +1100

    Docstring for check_theme and main_theme function. Specifications for both functions as well as description of return values

--- scopus_api_retrive/apply_theme.py
+++ scopus_api_retrive/apply_theme.py
@@ -6,6 +6,19 @@ Created on Mon Oct  8 10:54:44 2018
 """
 
 def check_theme(theme_subject):
+    """Helper function which can help to identify scopus subject catgories set is
+      a disjoint set of author subject catgories.
+     
+    Parameter
+    ----------
+    theme_subject : str
+                  Three main subject categories of Scopus author Subject areas
+    Return
+    ----------
+    theme : str
+           Mapping of the theme name
+           
+    nomatch : None """ 
     
     cancer = {'Cancer Research','Oncology', 'Cancer', 'Radiation'}
 
@@ -24,11 +37,26 @@ def check_theme(theme_subject):
 
 
 def main_theme(subjects):
+    """This is a custom function to map Scopus author subject categories to 
+    pre-defined theme
     
+    Parameter
+    ----------
+    subjects : tuple
+              Scopus author subject categories
+    Return
+    ----------
+    theme, result : tuple
+                    theme is the mapping of subject sub heading
+                    result is a set of string which has matched to author subject
+                    All/Any theme 
+                   default mapping if no mactch is found
+    """                       
     d = {'Cancer' : {'Cancer Research','Oncology', 'Cancer', 'Radiation'} ,
-         'Triple I' : {'Endocrinology','Immunology and Microbiology','Immunology', 'Microbiology',
-             'Parasitology', 'Virology', 'Dermatology', 'Allergy', 'Infectious Diseases',
-             'Rheumatology', 'Toxicology'}}
+         'Triple I' : {'Endocrinology','Immunology and Microbiology','Immunology', 
+                       'Microbiology','Parasitology', 'Virology', 'Dermatology', 
+                       'Allergy', 'Infectious Diseases','Rheumatology',
+                       'Toxicology'}}
     main_subject1, main_subject2, main_subject3, *alternatives = subjects 
      
     theme_subject = {main_subject1, main_subject2, main_subject3}
