c57c7ae00b8d692c3b8c0f345c377f4007c4c94a
Sudipta Paul <sudipta116@gmail.com>
2018-10-08 15:01:47 +1100

    Author current affliation, primary theme, alternative theme and results variables are included in the script

--- scopus_api_retrive/scival_author_subjects.py
+++ scopus_api_retrive/scival_author_subjects.py
@@ -4,34 +4,30 @@ Created on Tue Oct  2 10:51:59 2018
 
 @author: z3525552
 """
-
-
 def author_subject_area(SCOPUS_IDs):
     
     import pandas as pd
     from scopus_authors_retrive import scopus_author
     from collections import defaultdict
+    from apply_theme import main_theme
     
     scopus_id = defaultdict(list)
   
-   
     for author in SCOPUS_IDs:
-       
-       scopus_id['SCOPUS_ID'].append(author)
+        scopus_id['SCOPUS_ID'].append(author)
        #Retriving author from SCOPUS
-       au = scopus_author(author)
-       
-       scopus_id['Name'].append(au.name)
-       subjects, documents = zip(*au.categories)
-       
-       scopus_id['subject_categorties'].append(subjects)
-       scopus_id['document_number'].append(documents)
+        au = scopus_author(author)
+        subjects, documents = zip(*au.categories)
+        primary_theme, result = main_theme(subjects)
+        scopus_id['Name'].append(au.name)      
+        scopus_id['organisation'].append(au.current_affiliation)      
+        scopus_id['Main_theme'].append(primary_theme)
+        scopus_id['Alternative_theme'].append(' ')
+        scopus_id['Result'].append(result)
+        scopus_id['Subject_category'].append(subjects)
+        scopus_id['document_number'].append(documents)
     ## Add columns for each 'Theme' and tranfrom subjects to match 'Theme'   
        
-       
-       
     df = pd.DataFrame.from_dict(scopus_id)
-    
-    
     
     return df
