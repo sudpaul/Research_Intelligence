f04839d2015294c8186d0e228bc56cfde9b92e78
Sudipta Paul <sudipta116@gmail.com>
2018-10-09 14:18:22 +1100

    docstring for the nuroscience_theme function. Minor name change data dictonary d varibales to data

--- scopus_api_retrive/nuroscience_subjects.py
+++ scopus_api_retrive/nuroscience_subjects.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+__# -*- coding: utf-8 -*-
 """
 Created on Tue Oct  9 11:01:27 2018
 
@@ -8,7 +8,21 @@ Created on Tue Oct  9 11:01:27 2018
 
 def nuroscience_theme(subjects):
     
-    d = {'NHMA' : {'Clinical Neurology','Psychiatry and Mental Health', 'General Neuroscience',
+    """This is a custom function to map Scopus author subject area of 
+    nuroscience to pre-defined theme
+    Parameters
+    ----------
+    subjects : tuple
+              Scopus author subject categories
+    Returns
+    ----------
+    theme, result : tuple
+                    theme is the mapping of subject sub heading to
+                    aggregate higher level
+                    result is a set of string which has matched to author subject
+                    All/Any theme 
+                   default mapping if no mactch is found"""
+    data = {'NHMA' : {'Clinical Neurology','Psychiatry and Mental Health', 'General Neuroscience',
                    'Neuroscience (miscellaneous)' , 'Behavioral Neuroscience', 
                    'Biological Psychiatry','Cellular and Molecular Neuroscience','Cognitive Neuroscience',
                    'Developmental Neuroscience', 'Neurology' ,'Phychiatric Mental Health','Psychology (miscellaneous)',
@@ -18,8 +32,8 @@ def nuroscience_theme(subjects):
         
     theme_subject = set(subjects)
     
-    if not d['NHMA'].isdisjoint(theme_subject):       
-        result =  d['NHMA'].intersection(theme_subject)
+    if not data['NHMA'].isdisjoint(theme_subject):       
+        result =  data['NHMA'].intersection(theme_subject)
         return 'NHMA', result
     else :
         return 'All/Any theme', None
