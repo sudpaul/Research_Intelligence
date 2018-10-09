23aa388d8ee982752d5df749f69101b41f3ee3fe
Sudipta Paul <sudipta116@gmail.com>
2018-10-09 12:35:57 +1100

    Update docstring for check_theme function. Five main subject areas inculdes in the docstring. Fix minor error on else statesments, return value None was missing.

--- scopus_api_retrive/apply_theme.py
+++ scopus_api_retrive/apply_theme.py
@@ -6,6 +6,22 @@ Created on Mon Oct  8 10:54:44 2018
 """
 def check_theme(theme_subject):
     
+    """Helper function which can help to identify scopus subject catgories set is
+      a disjoint set of author subject catgories.
+     
+    Parameters
+    ----------
+    theme_subject : str
+                  Five main subject categories of Scopus author Subject areas
+    Returns
+    ----------
+    theme : str
+           Mapping of the theme name
+           
+    nomatch : None """ 
+    
+    
+    
     cancer = {'Cancer Research','Oncology', 'Cancer', 'Radiation'}
 
     Triple_I = { 'Endocrinology','Immunology and Microbiology','Immunology', 'Microbiology',
@@ -30,6 +46,20 @@ def check_theme(theme_subject):
 
 def main_theme(subjects):
     
+    """This is a custom function to map Scopus author subject categories to 
+    pre-defined theme
+    Parameters
+    ----------
+    subjects : tuple
+              Scopus author subject categories
+    Returns
+    ----------
+    theme, result : tuple
+                    theme is the mapping of subject sub heading
+                    result is a set of string which has matched to author subject
+                    All/Any theme 
+                   default mapping if no mactch is found
+    """               
     d = {'Cancer' : {'Cancer Research','Oncology', 'Cancer', 'Radiation'} ,
          'Triple I' : {'Endocrinology','Immunology and Microbiology','Immunology', 'Microbiology',
              'Parasitology', 'Virology', 'Dermatology', 'Allergy', 'Infectious Diseases',
@@ -48,4 +78,4 @@ def main_theme(subjects):
         result =  d[theme].intersection(theme_subject)
         return theme, result
     else:
-        return "All/Any theme", 
+        return "All/Any theme", None
