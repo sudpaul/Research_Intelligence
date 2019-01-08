5de3924deffe1e6e70a6209688807254ddc49d63
Sudipta Paul <sudipta116@gmail.com>
2019-01-08 14:06:18 +1100

    Add author_publications script to retrive publication history from Scopus

---
+++ scopus_api_retrive/author_publications.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  8 13:57:33 2019
+
+@author: z3525552
+"""
+
+def author_publication(scopus_id):
+    
+    """Retriving Journal publication detail from Scopus 
+    input scopus_id for which  return publication history
+   
+    Parameter
+    ----------
+    scopus_id : str
+               Author Scopus id
+               
+    """
+    from scopus import ScopusAuthor
+    import pandas as pd     
+
+    researcher = ScopusAuthor(6701859739)
+    journals = pd.DataFrame(researcher.publication_history) 
+    journals.columns = ['sourcetitle','abbreviation','publication_type','issn']
+
+    return journals      
