937a0e500d0d9661b18fe9db90942a7239155467
Sudipta Paul <sudipta116@gmail.com>
2019-01-10 15:00:55 +1100

    Export the result using CSS selector and add try block

--- scopus_api_retrive/scopus_co-authors_download.py
+++ scopus_api_retrive/scopus_co-authors_download.py
@@ -7,30 +7,34 @@ Created on Mon Dec 17 15:15:17 2018
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-# get the path of IEDriverServer
-#dir = os.path.dirname(__file__)
-driver = webdriver.Chrome(executable_path="C:/Users/z3525552/Downloads/chromedriver_win32/chromedriver.exe")
-driver.get('https://www.scopus.com/authid/detail.url?authorId=6603573607')
+try:
+   driver = webdriver.Chrome(executable_path="C:/Users/z3525552/Downloads/chromedriver_win32/chromedriver.exe")
+   driver.get('https://www.scopus.com/authid/detail.url?authorId=6603573607')
+   driver.implicitly_wait(5)
 
-co_authors = driver.find_element_by_id('coAuthLi')
-co_authors.click()
+   co_authors = driver.find_element_by_id('coAuthLi')
+   co_authors.click()
+   driver.implicitly_wait(3)
 
-overview = driver.find_element_by_link_text('View co-author overview')
-overview.click()
+   overview = driver.find_element_by_link_text('View co-author overview')
+   overview.click()
+   driver.implicitly_wait(2)
 
-export = driver.find_element_by_id("export_results")
-export.click()
+   export = driver.find_element_by_id("export_results")
+   export.click()
+   driver.implicitly_wait(2)
 
-csv_export = driver.find_element_by_class_name("radio")
-csv_export.click()
+   csv_export = driver.find_element_by_class_name("radio")
+   csv_export.click()
+   driver.implicitly_wait(1)
 
-#result = driver.find_element_by_tag_name("button")
-#result.click()
+   result = driver.find_element_by_css_selector("button.btn.btn-primary.btn-sm.pull-right.dropdownActionButton.exportButton") 
+   result.click()
+    
+finally:
+        driver.close()
 
-driver.close()
 
 
 
-#<button class="btn btn-primary btn-sm pull-right dropdownActionButton exportButton" type="button"><span class="btnText">Export</span></button>
 
-#test xpath //*[@id="export_results-data"]/span[2]/span/button[2]/span 
