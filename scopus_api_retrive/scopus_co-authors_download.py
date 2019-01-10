6383f844ef9084dbcb87de85a72b5bba5aa539d1
Sudipta Paul <sudipta116@gmail.com>
2019-01-10 15:15:10 +1100

    Remove test SCOPUS_ID and add user input SCOPUS_ID and changing the implicity timer

--- scopus_api_retrive/scopus_co-authors_download.py
+++ scopus_api_retrive/scopus_co-authors_download.py
@@ -7,15 +7,19 @@ Created on Mon Dec 17 15:15:17 2018
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+link = 'https://www.scopus.com/authid/detail.url?authorId='
+
+scopus_id = input('Please enter author SCOPUS Id')
+url = link+scopus_id
+
 try:
    driver = webdriver.Chrome(executable_path="C:/Users/z3525552/Downloads/chromedriver_win32/chromedriver.exe")
-   driver.get('https://www.scopus.com/authid/detail.url?authorId=6603573607')
+   driver.get(url)
    driver.implicitly_wait(5)
 
    co_authors = driver.find_element_by_id('coAuthLi')
    co_authors.click()
-   driver.implicitly_wait(3)
-
+   
    overview = driver.find_element_by_link_text('View co-author overview')
    overview.click()
    driver.implicitly_wait(2)
