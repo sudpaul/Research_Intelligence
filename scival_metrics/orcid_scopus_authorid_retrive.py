125795d170b377ae0e23d9e63f7d705eee38e9e5
Sudipta Paul <sudipta116@gmail.com>
2019-01-14 13:42:01 +1100

    Write function to convert ORCID to SCOPUS author id

--- scival_metrics/orcid_scopus_authorid_retrive.py
+++ scival_metrics/orcid_scopus_authorid_retrive.py
@@ -5,25 +5,31 @@ Created on Mon Jan 14 12:22:08 2019
 @author: z3525552
 """
 
-import requests
+
+def orcid_scopusid(ORCID):
     
-key = 'cf19ff27ef3c0d95f93c26947eb6533f'
-    
-base_url ='https://api.elsevier.com/content/author/orcid/0000-0002-3612-8298'
-    
-header = {'Accept':'application/json', 'X-ELS-APIKey': key}
-    
-try:
-    response = requests.get(url=base_url, headers=header)
-    
-    if not response.status_code == 200:
+    import requests
         
-        response.status_code
-    else:
-   
+    with open('../elsevier_developer') as f:
+            token = f.read().strip()
+    
+    
+        
+    base_url ='https://api.elsevier.com/content/author/orcid/{orcid}'.format(orcid=ORCID)
+        
+    header = {'Accept':'application/json', 'X-ELS-APIKey': token}
+        
+    try:
+        response = requests.get(url=base_url, headers=header)
+        
+        if not response.status_code == 200:
+            
+           return response.status_code
+    
         response_data = response.json()       
         result = response_data['author-retrieval-response'][0]['coredata']                      
-        response_data
-    
-except:
-       0
+        _, scopus_id = result['dc:identifier'].split(':')
+        return scopus_id
+        
+    except:
+        return 'Not Found'
