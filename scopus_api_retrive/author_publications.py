466d6ba4f2d821d255a78718c1762679c257e8c0
Sudipta Paul <sudipta116@gmail.com>
2019-01-15 16:41:50 +1100

    test script for co-authors data

--- scopus_api_retrive/author_publications.py
+++ scopus_api_retrive/author_publications.py
@@ -49,3 +49,45 @@ def publication_subjects(scopus_id):
     subjects = subjects.sort_values(by=['n_documents'], ascending=False)
     
     return subjects
+
+def get_coauthors(scopus_id):
+    """Retrieves basic information about co-authors as a list of
+        namedtuples in the form
+        name, scopus_id, affiliation, categories
+        where areas is a list of subject area codes joined by "; ".
+        Note: These information will not be cached and are slow for large
+        coauthor groups.
+    """
+    from collections import namedtuple
+        # Get number of authors to search for
+    res = scopus_author(scopus_id)
+    data = res.get_coauthors()
+        
+        # Store information in namedtuples
+    fields = 'surname given_name id areas affiliation_id name city country'
+    coauth = namedtuple('Coauthor', fields)
+    coauthors = []
+        # Iterate over search results in chunks of 25 results
+    count = 0
+    while count < len(data):
+        params = {'start': count, 'count': 25}
+        res = download(url=self.coauthor_link, params=params, accept='json')
+        data = loads(res.text)['search-results'].get('entry', [])
+        # Extract information for each coauthor
+        for entry in data:
+            aff = entry.get('affiliation-current', {})
+            try:
+                areas = [a['$'] for a in entry.get('subject-area', [])]
+            except TypeError:  # Only one subject area given
+                areas = [entry['subject-area']['$']]
+            new = coauth(surname=entry['preferred-name']['surname'],
+                         given_name=entry['preferred-name'].get('given-name'),
+                         id=entry['dc:identifier'].split(':')[-1],
+                         areas='; '.join(areas),
+                         affiliation_id=aff.get('affiliation-id'),
+                         name=aff.get('affiliation-name'),
+                         city=aff.get('affiliation-city'),
+                         country=aff.get('affiliation-country'))
+            coauthors.append(new)
+        count += 25
+    return coauthors
