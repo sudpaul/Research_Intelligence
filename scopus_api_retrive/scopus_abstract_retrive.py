ff419e7bde17f72b0495101c3aa7f79d03fdca0a
Sudipta Paul <sudipta116@gmail.com>
2019-02-21 16:12:16 +1100

    Add function to retrive author scopus_ids from abstract'

--- scopus_api_retrive/scopus_abstract_retrive.py
+++ scopus_api_retrive/scopus_abstract_retrive.py
@@ -9,24 +9,21 @@ Created on Thu Feb 21 13:13:28 2019
 import requests
 import pandas as pd
 
-     
-with open('../elsevier_developer') as f:
-            token = f.read().strip()    
-base_url ='https://api.elsevier.com/content/abstract/eid/2-s2.0-85012915011'
+def eid_authorid(eid):     
+    
+    with open('../elsevier_developer') as f:
+                token = f.read().strip()    
+    base_url =f'https://api.elsevier.com/content/abstract/eid/{eid}'
+            
+    header = {'Accept':'application/json', 'X-ELS-APIKey': token}
+    res = requests.get(url=base_url, headers=header)
+    
+    response = res.json() 
+        # Get authors of the abstracts
         
-header = {'Accept':'application/json', 'X-ELS-APIKey': token}
+    data = response["abstracts-retrieval-response"]["authors"]["author"]
+    scopus_id = [author['@auid'] for author in data]
+     
+       
+    return scopus_id
 
-res = requests.get(url=base_url, headers=header)
-
-response = res.json() 
-
-
-# Get authors of the abstracts
-
-data = pd.DataFrame.from_records(response['abstracts-retrieval-response']['authors']['author'])
-
-
-
-df =  data[['ce:surname','@auid']]
-
-df = df.rename(columns={'@auid': 'Scopus_id', 'ce:surname': 'Surname'})
