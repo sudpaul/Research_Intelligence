3c82ef34d828a4423163e71b797c7509491c5cce
Sudipta Paul <sudipta116@gmail.com>
2019-02-26 14:11:39 +1100

    Remove filepath input parameter and inculde the developer file location of api token in the function

--- scopus_api_retrive/scopus_doi_edi.py
+++ scopus_api_retrive/scopus_doi_edi.py
@@ -5,8 +5,7 @@ Created on Thu Aug 23 13:44:21 2018
 @author: z3525552
 """
 
-def doi_edi(doi, filepath):
-    
+def doi_edi(doi):    
     ''' This function is used to change SCOPUS DOI to EDI number
     
     Parameters
@@ -14,22 +13,21 @@ def doi_edi(doi, filepath):
     doi : str 
           Scopus' Document Object Identifier 
     
-    filepath : str
-               Elsiver API key Filepath
+    
     Return
     -------
     edi : str
           Scopus’ Electronic Identifier'''      
     import requests
     
-    from elsiver_api_retrive import get_elseiver_API
+    with open('../elsevier_developer') as f:
+                token = f.read().strip()    
     
-    key = get_elseiver_API(filepath) 
     
     base_url ='http://api.elsevier.com/content/search/scopus?query=%s'%(doi)
     
     
-    header = {'Accept':'application/json', 'X-ELS-APIKey': key}
+    header = {'Accept':'application/json', 'X-ELS-APIKey': token}
     
     try:
         response = requests.get(url=base_url, headers=header)
