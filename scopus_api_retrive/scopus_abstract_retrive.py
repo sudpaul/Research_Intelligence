36f026102e590238da8f2b0d485f000c345160f2
Sudipta Paul <sudipta116@gmail.com>
2019-04-05 15:39:12 +1100

    grants meta data extract from publication

--- scopus_api_retrive/scopus_abstract_retrive.py
+++ scopus_api_retrive/scopus_abstract_retrive.py
@@ -8,22 +8,45 @@ Created on Thu Feb 21 13:13:28 2019
 
 import requests
 
-def eid_authorid(eid):     
+
+
+def publication_retrive(eid):
     
-    with open('../elsevier_developer') as f:
-                token = f.read().strip()    
+    #with open('../elsevier_developer') as f:
+               # token = f.read().strip()    
     base_url =f'https://api.elsevier.com/content/abstract/eid/{eid}'
             
-    header = {'Accept':'application/json', 'X-ELS-APIKey': token}
+    header = {'Accept':'application/json', 'X-ELS-APIKey': 'cf19ff27ef3c0d95f93c26947eb6533f'}
     res = requests.get(url=base_url, headers=header)
     
     response = res.json() 
-        # Get authors of the abstracts
         
-    data = response["abstracts-retrieval-response"]["authors"]["author"]
+    return response
+
+
+
+
+
+def eid_authorid(eid):     
+    
+    res_obj = publication_retrive(eid)
+    # Get authors of the abstracts    
+    data = res_obj["abstracts-retrieval-response"]["authors"]["author"]
     auth_id = {author['ce:surname']:author['@auid'] for author in data}
      
        
     return auth_id
 
 #To do generalise abstract retrive to get funding agency from dois/eids/pubmed_id
+    
+
+def eid_grants(eid):
+    
+    res_obj = publication_retrive(eid)
+    funding = res_obj["abstracts-retrieval-response"]["item"]["xocs:meta"]["xocs:funding-list"]["xocs:funding"]
+    
+    grants = {}
+    for fund in funding:
+        grants[fund["xocs:funding-id"]]= fund["xocs:funding-agency"]
+    
+    return grants
