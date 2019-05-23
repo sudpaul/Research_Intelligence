41e52eb56b127df103e66e99d6824cb283ff50a6
Sudipta Paul <sudipta116@gmail.com>
2019-05-23 15:17:34 +1000

    Update publication subjects as same as ScopusAuthor

--- scopus_api_retrive/author_subjects.py
+++ scopus_api_retrive/author_subjects.py
@@ -3,14 +3,10 @@
 Created on Thu May 23 12:52:38 2019
 
 @author: z3525552
-"""
 
-# change the datatype to np.int of publication count 
-    
-# sort the data key publication count
-    
-# map to dictionary
-    
+Upgrade scopus from 0.8 to 1.6 deprecated support ScopusAuthor and
+introduction of new class AuthorRetieval
+"""
 def get_subject_docs(identifier, refresh):
     """Returns (subject area, number of documents)-tuples."""
     
@@ -18,9 +14,12 @@ def get_subject_docs(identifier, refresh):
     from operator import itemgetter
     
     author = AuthorRetrieval(identifier, refresh=refresh)
+    # publication number mapping to ASJC code dictionary
     docs = dict(author.classificationgroup)
+    #Retrive the names and number of publication from author subject areas
     names = [(publication.area, int(docs[publication.code])) for publication in author.subject_areas]
-    
+      
+    # sort the data key publication count
     names.sort(reverse=True, key=itemgetter(1))
     
     publications = dict(names)    
