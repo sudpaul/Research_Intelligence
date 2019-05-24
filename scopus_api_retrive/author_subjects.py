6f3472a09d1bddbcd1ce0d66cb5cce2b48a11c5b
Sudipta Paul <sudipta116@gmail.com>
2019-05-24 11:25:29 +1000

    import scopus_author from scopus_authors_retrive to the script

--- scopus_api_retrive/author_subjects.py
+++ scopus_api_retrive/author_subjects.py
@@ -7,13 +7,13 @@ Created on Thu May 23 12:52:38 2019
 Upgrade scopus from 0.8 to 1.6 deprecated support ScopusAuthor and
 introduction of new class AuthorRetieval
 """
-def get_subject_docs(identifier, refresh):
+def get_subject_docs(identifier):
     """Returns (subject area, number of documents)-tuples."""
     
-    from scopus import AuthorRetrieval
+    from scopus_authors_retrive import scopus_author
     from operator import itemgetter
     
-    author = AuthorRetrieval(identifier, refresh=refresh)
+    author = scopus_author(identifier)
     # publication number mapping to ASJC code dictionary
     docs = dict(author.classificationgroup)
     #Retrive the names and number of publication from author subject areas
